65278375d4bc20e284d90517404ad54d426ddbc8
Jayden Liang <jaydenliang81@gmail.com>
2015-09-01 20:42:32 -0700

    Initial Commit

---
+++ EasyCoding/EasyCoding.h
@@ -0,0 +1,13 @@
+//
+//  EasyCoding.h
+//  EasyCoding
+//
+//  Created by Jayden Liang on 15/9/1.
+//  Copyright (c) 2015年 BCOZ Online. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EasyCoding : NSObject
+
+@end
---
+++ EasyCoding/EasyCoding.m
@@ -0,0 +1,13 @@
+//
+//  EasyCoding.m
+//  EasyCoding
+//
+//  Created by Jayden Liang on 15/9/1.
+//  Copyright (c) 2015年 BCOZ Online. All rights reserved.
+//
+
+#import "EasyCoding.h"
+
+@implementation EasyCoding
+
+@end
